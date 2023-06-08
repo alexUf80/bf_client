d89b66cfa306138234e66fec271e92443fb95928
alexUf80 <50929505+alexUf80@users.noreply.github.com>
2023-06-08 16:05:51 +0300

    Отоброжение пролонгации у клиента

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -332,7 +332,10 @@ class AccountController extends Controller
                 }
             }
 
-            if (date_diff($now, $start_date)->days >= 7 && $count_prolongation < 5) {
+            if (date_diff($now, $start_date)->days >= 7 && $count_prolongation < 5
+            && date_diff($now, $start_date)->days < 100
+            && (($order->contract->loan_body_summ * 1.5) > $order->contract->loan_percents_summ)
+            ) {
                 $show_prolongation = 1;
             }
             $pro_date = new DateTime(date('Y-m-d', strtotime($order->contract->return_date)));
